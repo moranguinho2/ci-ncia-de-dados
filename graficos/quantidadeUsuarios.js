4c3a2c9faec8e5a1a96f8733eca3e6854117bdc8
kalyane.martins <105065767+moranguinho2@users.noreply.github.com>
2024-10-21 11:24:27 -0300

    21.10.24
    aula 1

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -20,7 +20,34 @@ color: getCSS('--primary-color')
 
 const layout = {
 plot_bgcolor: getCSS('--bg-color'),
-paper_bgcolor: getCSS('--bg-color')
+paper_bgcolor: getCSS('--bg-color'),
+title: {
+    text: 'Redes sociais que os usuários mais gostam',
+    x:0,
+    font: {
+        color: getCSS('--primary-color'),
+        family: getCSS('--font'),
+        size: 30
+    }
+}, 
+xaxis: {
+    tickfont: tickConfig,
+    title: {
+        text: 'nome das redes sociais',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+},
+yaxis: {
+    tickfont: tickConfig,
+    title: {
+        text: 'bilhões de usuários ativos',
+        font: {
+            color: getCSS('--secondary-color')
+        }
+    }
+}
 
 }
 
