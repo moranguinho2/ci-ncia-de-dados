ea4101747f1e2c0ccac1c607ebd3ed9b0c4b5676
kalyane.martins <105065767+moranguinho2@users.noreply.github.com>
2024-11-11 11:37:19 -0300

    11.11.24

--- graficos/common.js
+++ graficos/common.js
@@ -1,30 +1,31 @@
 const getCSS = (variavel) => {
     return getComputedStyle(document.body).getPropertyValue(variavel)
-}
-
-const tickConfig = {
+    }
+    
+    const tickConfig = {
     color: getCSS('--primary-color'),
     size: 16,
     family: getCSS('--font')
-}
-
-function criarGrafico (data, layout) {  
+    }
+    
+    function criarGrafico (data, layout) {
     const grafico = document.createElement ('div')
     grafico.className = 'grafico'
     document.getElementById ('graficos-container').appendChild (grafico)
     const config= {
-        responive: true,
-        displayModeBar: false
+    responive: true,
+    displayModeBar: false
     }
     Plotly.newPlot (grafico, data, layout, config)
-}
-
-function incluirTexto (texto) {
+    }
+    
+    function incluirTexto (texto) {
     const container = document.getElementById ('graficos-container')
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
     paragrafo.innerHTML = texto
     container.appendChild(paragrafo)
-}
-
-export { getCSS, tickConfig, criarGrafico, incluirTexto }
+    }
+    
+    export { getCSS, tickConfig, criarGrafico, incluirTexto }
+    
